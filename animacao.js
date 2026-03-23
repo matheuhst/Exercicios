127eda3fe7eca19e18eeb3fa2052a56b3dcf8fc9
matheuhst <mmmatheus.fsouza@gmail.com>
2026-03-23 13:55:25 -0300

    alterações necessárias

--- animacao.js
+++ animacao.js
@@ -2,8 +2,21 @@ function mudar(estado) {
     const quadrado = document.querySelector('.quadrado');
 
     // remove os estados antigos
-    quadrado.classList.remove('feliz', 'triste');
+    quadrado.classList.remove('feliz', 'triste', 'nervoso');
 
     // adiciona o novo estado
     quadrado.classList.add(estado);
 }
+
+let primeiraInteracao = true;
+
+const quadrado = document.querySelector('.quadrado');
+
+quadrado.addEventListener('mouseenter', () => {
+    if (primeiraInteracao) {
+        mudar('nervoso');
+        primeiraInteracao = false;
+    } else {
+        mudar('feliz');
+    }
+});
